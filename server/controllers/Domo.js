375061d26c9e0717ace1b49db33ad1918984335f
NicholasPaniccioli <njp7745@g.rit.edu>
2020-10-25 18:52:28 -0400

    finished DomoB

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,58 +1,16 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const _ = require('underscore');
-
 const models = require ('../models');
+
 const Domo = models.Domo;
 
-let DomoModel = {};
-
-const convertId = mongoose.Types.ObjectId;
-const setName = (name) => _.escape(name).trim();
-
-const DomoSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        set: setName,
-    },
-
-    age: {
-        type: Number,
-        min: 0,
-        required: true,
-    },
-
-    owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-    },
-
-    createdData: {
-        type: Date,
-        default: Date.now,
-    },
-});
-
-DomoSchema.statics.toAPI = (doc) => ({
-    name: doc.name,
-    age: doc.age,
-});
-
-DomoSchema.statics.findByOwner = (ownerId, callback) => {
-    const search = {
-        owner: convertId(ownerId),
-    };
-
-    return DomoModel.find(search).select('name age').lean().exec(callback);
-};
-
-DomoModel = mongoose.model('Domo', DomoSchema);
-
 const makerPage = (req, res) => {
-    res.render('app');
+    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs)=> {
+        if(err) {
+            console.log(err);
+            return res.status(400).json({ error: 'An error occurred'});
+        }
+
+        return res.render('app', {domos: docs});
+    });
 };
 
 const makeDomo = (req, res) => {
@@ -84,7 +42,5 @@ const makeDomo = (req, res) => {
     return domoPromise;
 };
 
-module.exports.DomoModel = DomoModel;
-module.exports.DomoSchema = DomoSchema;
 module.exports.makerPage = makerPage;
 module.exports.make = makeDomo;
