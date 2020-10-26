55705518c19cb2e786994b27cbbcb52e12428754
NicholasPaniccioli <njp7745@g.rit.edu>
2020-10-25 20:19:34 -0400

    started DomoMaker-C, step 14

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -23,6 +26,23 @@ mongoose.connect(dbURL, mongooseOptions, (err) =>{
     }
 });
 
+let redisURL = {
+    hostname: 'redis-14915.c232.us-east-1-2.ec2.cloud.redislabs.com',
+    port: '14915',
+};
+
+let redisPASS = 'GbLDmO1AhQ3tyM52ZNoJQB2XzYGWBRnu';
+if(process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    [,redisPass] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPASS,
+});
+
+
 const router = require ('./router.js');
 
 const app = express();
@@ -34,9 +54,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
     key: 'sessionid',
+    store: new RedisStore({
+        client: redisClient,
+    }),
     secret: 'Domo Arigato',
     resave: true,
     saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+    },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
